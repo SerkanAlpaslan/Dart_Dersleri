7c57530be9b3107b45d7c57471b483f39c288219
SerkanAlpaslan <serkanalpaslann@gmail.com>
2021-10-27 18:20:55 +0300

    27.10 duzenleme

---
+++ bolum6/3kalitim_ornek.dart
@@ -0,0 +1,17 @@
+main(List<String> args) {}
+
+class Kisi {
+  int yas;
+  String isim;
+
+  Kisi(this.isim, this.yas);
+  void kendiniTanit() {
+    print("Adım $isim, yaşım $yas");
+  }
+}
+
+class Calisan extends Kisi {
+  int maas;
+
+  Calisan(String name, int age,this.maas) : super(name, age);//ALT KATMANA ÖZEL OLAN PARAMETRELERDE ONU this METODU İLE KURUCUYA EKLEMEMİZ GEREKLİDİR.
+}

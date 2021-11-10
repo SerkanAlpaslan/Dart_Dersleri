849a8e3d467a927f9a65003123d2ac19e31d2fa6
SerkanAlpaslan <serkanalpaslann@gmail.com>
2021-11-10 18:39:48 +0300

    .

--- bolum8/2higher_order_fonksiyonları.dart
+++ bolum8/2higher_order_fonksiyonları.dart
@@ -1,1 +1,20 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  List<int> liste = [1, 2, 3];
+  /*liste.forEach((element) {
+    print("element: $element");
+  });*/
+  liste.forEach(callback);
+  kendiForEachYapim(liste, (int deger, int index) {
+    print("Değer $deger");
+  });
+}
+
+void kendiForEachYapim(List<int> liste, Function callback) {
+  for (int i = 0; i < liste.length; i++) {
+    callback(liste[i], i);
+  }
+}
+
+void callback(int element) {
+  print("Element $element");
+}

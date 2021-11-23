0d39109a0b479c4071a2c76a6c479a277d27dcd6
SerkanAlpaslan <serkanalpaslann@gmail.com>
2021-11-23 11:15:59 +0300

    ghj

--- bolum9/5async_await.dart
+++ bolum9/5async_await.dart
@@ -1,7 +1,14 @@
-void main(List<String> args) {
+void main(List<String> args) async {
+  //await diyoruz beklemesi çin. Sonra asyn yapıyoruız fonnkdiyonu yoksa çalşmıypr. Buunu bu şekidle fonksiyonnunu da bunuun içerisnde olacak şekilde kullanırsan awaitten dolayı önce veri gelir sonra altındaki diğer kodlar çalışır. Ondan dolayo başkla fonksiyonda çalştırıısz
+
   print("İntertnnertin kişi verisi getireilcek");
-  String kisi = await kisiVerisiniGetiri(); //.then((value) => print(value));
+  kisiyleIlgiliIslemler();
   print("işlem bitti");
+}
+
+void kisiyleIlgiliIslemler() async {
+  String kisi = await kisiVerisiniGetiri();
+  print(kisi.length);
 }
 
 Future<String> kisiVerisiniGetiri() {
